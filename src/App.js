ee8f0824b7975324e19531c5b4b1a1cd9b082e09
msanvarov <salim.anvarov@yahoo.com>
2019-05-02 13:42:22 -0400

    styling changes to fix particle js not covering the whole background

--- src/App.js
+++ src/App.js
@@ -80,6 +80,60 @@ class App extends Component {
         <Aux>
           <Preloader show={this.state.showLoader} />
           <div className="prt_main_wrapper">
+            <Particles
+              params={{
+                particles: {
+                  number: {
+                    value: 200,
+                    density: {
+                      enable: false
+                    }
+                  },
+                  size: {
+                    value: 3,
+                    random: true,
+                    anim: {
+                      speed: 4,
+                      size_min: 0.3
+                    }
+                  },
+                  line_linked: {
+                    enable: false
+                  },
+                  move: {
+                    random: true,
+                    speed: 1,
+                    direction: "top",
+                    out_mode: "out"
+                  }
+                },
+                interactivity: {
+                  events: {
+                    onhover: {
+                      enable: true,
+                      mode: "bubble"
+                    },
+                    onclick: {
+                      enable: true,
+                      mode: "repulse"
+                    }
+                  },
+                  modes: {
+                    bubble: {
+                      distance: 250,
+                      duration: 2,
+                      size: 0,
+                      opacity: 0
+                    },
+                    repulse: {
+                      distance: 400,
+                      duration: 4
+                    }
+                  }
+                }
+              }}
+              style={{ zIndex: "-1", position: "fixed" }}
+            />
             <div
               className="prt_home_wrapper"
               style={{ height: window.innerHeight }}
@@ -87,59 +141,6 @@ class App extends Component {
               <h1 className="d-flex justify-content-center">
                 I'm Salim Anvarov
               </h1>
-              <Particles
-                params={{
-                  particles: {
-                    number: {
-                      value: 200,
-                      density: {
-                        enable: false
-                      }
-                    },
-                    size: {
-                      value: 3,
-                      random: true,
-                      anim: {
-                        speed: 4,
-                        size_min: 0.3
-                      }
-                    },
-                    line_linked: {
-                      enable: false
-                    },
-                    move: {
-                      random: true,
-                      speed: 1,
-                      direction: "top",
-                      out_mode: "out"
-                    }
-                  },
-                  interactivity: {
-                    events: {
-                      onhover: {
-                        enable: true,
-                        mode: "bubble"
-                      },
-                      onclick: {
-                        enable: true,
-                        mode: "repulse"
-                      }
-                    },
-                    modes: {
-                      bubble: {
-                        distance: 250,
-                        duration: 2,
-                        size: 0,
-                        opacity: 0
-                      },
-                      repulse: {
-                        distance: 400,
-                        duration: 4
-                      }
-                    }
-                  }
-                }}
-              />
               <div className="prt_logo_wrapper">
                 <a href="#main">
                   <img src={Logo} alt="Logo" id="prt_close_tab" />

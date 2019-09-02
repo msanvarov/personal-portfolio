f66b5a9619ed1a4f4ee2d1878432e91a5ab925b8
msanvarov <salim.anvarov@yahoo.com>
2019-09-02 13:19:07 -0400

    proper import of circular progress bar

--- src/components/Capabilities/CircularProgressBar/CircularProgressBar.js
+++ src/components/Capabilities/CircularProgressBar/CircularProgressBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CircularProgressbar from "react-circular-progressbar";
+import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
 const CircularProgressBar = props => {

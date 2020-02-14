e3daa10992507df8ec40ab8a233f5b1577f8103c
msanvarov <salim.anvarov@yahoo.com>
2020-02-14 13:51:09 -0500

    :seedling: firebase/flamelink init file

---
+++ src/utils/firebase.ts
@@ -0,0 +1,24 @@
+import * as firebase from "firebase";
+import * as dotenv from "dotenv";
+import flamelink from "flamelink";
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const app = flamelink({
+  firebaseApp,
+  dbType: "rtdb", //  'rtdb' or 'cf' for Realtime DB or Cloud Firestore
+  env: "production",
+  locale: "en-US",
+  precache: true
+});

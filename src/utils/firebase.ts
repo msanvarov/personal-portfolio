cdd909face9cb17a4171d6a82af5d1bf4d92baab
msanvarov <msalanvarov@gmail.com>
2020-03-09 16:29:40 -0400

    :art: fixed firebase config

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -10,7 +10,8 @@ const firebaseConfig = {
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);

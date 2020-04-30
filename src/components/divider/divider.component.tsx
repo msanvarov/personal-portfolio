e7e67529e7202f725e94251d0c2b4ecaee81d7a7
msanvarov <msalanvarov@gmail.com>
2020-04-30 01:04:54 -0400

    :sparkles: divider component

---
+++ src/components/divider/divider.component.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components/macro';
+
+const defaultDividerProps = {
+  lineWidth: '100%',
+  lineHeight: '2px',
+  notchWidth: '90px',
+  notchHeight: '10px',
+  collapsed: false,
+  collapseDelay: 0,
+};
+
+type DividerProps = typeof defaultDividerProps;
+
+const Divider = styled.div<DividerProps>`
+  width: ${(props) => props.lineWidth};
+  height: ${(props) => props.lineHeight};
+  position: relative;
+  &::before {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: rgb(var(--rgbPrimary));
+    opacity: ${(props) => (props.collapsed ? 0 : 1)};
+    transition-property: transform, opacity;
+    transition-duration: 600ms;
+    transition-timing-function: var(--curveFastoutSlowin);
+    transition-delay: ${(props) => props.collapseDelay}ms;
+    transform: scaleX(${(props) => (props.collapsed ? 0 : 1)});
+    transform-origin: left center;
+  }
+  &::after {
+    content: '';
+    width: ${(props) => props.notchWidth};
+    height: ${(props) => props.notchHeight};
+    background: rgb(var(--rgbPrimary));
+    position: absolute;
+    top: ${(props) => props.lineHeight};
+    transition-property: clip-path, opacity;
+    transition-duration: 600ms;
+    transition-timing-function: var(--curveFastoutSlowin);
+    transition-delay: ${(props) => props.collapseDelay + 160}ms;
+    opacity: ${(props) => (props.collapsed ? 0 : 1)};
+    clip-path: ${(props) =>
+      props.collapsed
+        ? 'polygon(0 0, 0 0, 10px 100%, 10px 100%)'
+        : 'polygon(0 0, 100% 0, calc(100% - 10px) 100%, 10px 100%)'};
+  }
+`;
+
+Divider.defaultProps = defaultDividerProps;
+
+export default Divider;
